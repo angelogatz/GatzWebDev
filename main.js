6dfc4da6060b43745b2c5073096a614c50c392d6
Angelo Gatz <angeloagatz@gmail.com>
2021-12-01 15:17:38 -0300

    navbar responsiva e dark/light themes

--- main.js
+++ main.js
@@ -26,3 +26,33 @@ function scrollHeader(){
     if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+/*=============== Dark/Light Theme ===============*/
+const themeButton = document.getElementById('theme-btn')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+//Topico selecionado previamente (Se o usuário selecionar)
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedicon = localStorage.getItem('selected-icon')
+
+// Nos obtemos o tema em uso que foi validado pela interface na dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// Nós validamos se o usuário escolheu uma opção antes
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedicon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Ativar/Desativar o tema manualmente com o botão
+themeButton.addEventListener('click', () => {
+    // Adicionar ou remover o icone do tema escuro
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // Salvando o tema atual que foi escolhido pelo usuário
+    localStorage.setItem('selected-theme', getCurrentTheme)
+    localStorage.setItem('selected-icon', getCurrentIcon)
+})
